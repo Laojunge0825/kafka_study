96994bdcedb851675c4dc28f182d2ef60b2041c9
pengcanjun <1642287765@qq.com>
2025-02-11 15:46:10 +0800

    消费者接收对象消息，开启手动消息确认

--- springboot-kafka/src/main/java/com/shuke/springbootkafka/consumer/EventConsumer.java
+++ springboot-kafka/src/main/java/com/shuke/springbootkafka/consumer/EventConsumer.java
@@ -5,6 +5,7 @@ import cn.hutool.json.JSONUtil;
 import com.shuke.springbootkafka.model.User;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -14,7 +15,7 @@ import org.springframework.stereotype.Component;
 public class EventConsumer {
 
     // 采用监听的方法接收事件
-    @KafkaListener(topics = { "shukeTopic" } , groupId = "shukeGroup")
+//    @KafkaListener(topics = { "shukeTopic" } , groupId = "shukeGroup")
     /*
       @Payload 注解用于从消息负载中提取数据
      * 并将其绑定到方法参数上
@@ -32,5 +33,21 @@ public class EventConsumer {
         System.out.println("读取到的事件信息："+record.toString() + "/n" + ", 发送消息的时间：" + DateUtil.date(record.timestamp()));
         User user = JSONUtil.toBean(userJson, User.class);
         System.out.println("User对象：" + user.toString());
+    }
+
+    @KafkaListener(topics = { "shukeTopic" } , groupId = "shukeGroup")
+    public void onEvent2(@Payload String event,
+                         @Header(value = KafkaHeaders.RECEIVED_TOPIC) String topic,
+                         @Header(value = KafkaHeaders.RECEIVED_PARTITION) int partition,
+                         @Header(value = KafkaHeaders.OFFSET) int offset,
+                         Acknowledgment ack){
+
+        try {
+            System.out.println("读取到的事件：" + event + ", 主题：" + topic + ", 分区：" + partition + ", 偏移量：" + offset);
+            // 手动提交   手动确认消息
+            ack.acknowledge();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
